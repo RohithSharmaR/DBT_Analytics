665367a6b2e41897de7ccba316b5e720a02fda47
Rohith Sharma <rr03042001@gmail.com>
2025-08-30 23:53:34 +0000

    added dimension models

--- models/staging/stg_dmart_raw.sql
+++ models/staging/stg_dmart_raw.sql
@@ -1,3 +1,10 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+
 select 
 order_id,
 upper(customer_name) as CUSTOMER_NAME,
@@ -12,4 +19,5 @@ discount,
 profit,
 state ,
 from 
-deve_db.raw.dmart_raw
+{{ source('raw', 'dmart_raw') }}
+
